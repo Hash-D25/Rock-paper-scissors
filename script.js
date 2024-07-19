fa5c183dbff2d8d9f9970375301b607a29a12743
Hash-D25 <149588109+Hash-D25@users.noreply.github.com>
2024-07-20 03:33:54 +0530

    commit

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 let scores=JSON.parse(localStorage.getItem('scores'));
+
+function updatescoreline(){
+    document.querySelector('.scoreline').innerText=`Wins: ${scores.Wins}, Losses: ${scores.Loses}, Ties: ${scores.Ties}`;
+}
+function moves(){
+    document.querySelector('.moves').innerText=`Wins: ${scores.Wins}, Losses: ${scores.Loses}, Ties: ${scores.Ties}`;
+}
+function yourmove(){
+    document.querySelector('.youresult').innerHTML=`${playerMove}`;
+}
+function computermove(){
+    document.querySelector('.computerresult').innerHTML=`${computerMove}`;
+}
+updatescoreline();
 function score(){
+    updatescoreline();
     alert(`Scores:::wins:${scores.Wins} loses:${scores.Loses} ties:${scores.Ties}`);
 }
 if (!score) {
@@ -13,6 +28,7 @@ function reset(){
     scores.Wins=0;
     scores.Loses=0;
     scores.Ties=0;
+    updatescoreline();
     alert(`Scores reset:::wins:${scores.Wins} loses:${scores.Loses} ties:${scores.Ties}`);
 }
 function playgame(playerMove){
@@ -56,16 +72,14 @@ function playgame(playerMove){
     else if(result==='Tie'){scores.Ties+=1;}
     
     localStorage.setItem('scores',JSON.stringify(scores));
-    alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}
-        Wins: ${scores.Wins}, Losses: ${scores.Loses}, Ties: ${scores.Ties}`);
-             
+    updatescoreline();
+    document.querySelector('.result').innerHTML=result;
+    document.querySelector('.yourmove').innerHTML=`${playerMove}`;
+    document.querySelector('.computermove').innerHTML=`${computerMove}`;
+    
+    
     
 }
-    
-
-
-
-
 
 function pickComputerMove(){
     let computerMove='';
@@ -77,5 +91,6 @@ function pickComputerMove(){
     } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
         computerMove = 'scissors';
     }
+    
     return computerMove;
 }
